344d04adebc09a0c30b836c0e86445c594535a75
Marcelo Vieira <marcelovieira1995@gmail.com>
2023-02-20 10:17:22 -0300

    atuailzacao

---
+++ tema/templates/produtos/lipocavity-new-smart/content-14.php
@@ -0,0 +1,32 @@
+<section class="bloco-3-text <?php echo 'css-3-' . get_post_field('post_name', get_post()); ?>">
+    <div class="container">
+        <div class="init-bloco-3-text">
+            <div>
+                <h2> ##TEXTO 1 ## </h2>
+            </div>
+            <div>
+                <ul>
+                    <li>ITEM 1</li>
+                    <li>ITEM 2</li>
+                    <li>ITEM 3</li>
+                    <li>ITEM 4</li>
+                </ul>
+
+            </div>
+        </div>
+        <div class="init-bloco-3-text">
+            <div>
+                <h2> ##TEXTO 2 ## </h2>
+            </div>
+            <div>
+                <ul class="lista-estilo-1">
+                    <li>ITEM 1</li>
+                    <li>ITEM 2</li>
+                    <li>ITEM 3</li>
+                    <li class="active">ITEM 4</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</section>
