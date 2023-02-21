6e8a69fc9d055aac4835722354b7427ddf1b8d1a
Marcelo Vieira <marcelovieira1995@gmail.com>
2023-02-21 20:29:49 -0300

    Finalizado links do layout

--- tema/single-produtos.php
+++ tema/single-produtos.php
@@ -50,32 +50,32 @@
     <?php get_template_part(sprintf("templates/produtos/%s/content-12", get_post_field('post_name', get_post()))); ?>
 
     <div class="js-scroll fade-in-bottom">
-    <?php get_template_part(sprintf("templates/produtos/%s/content-9", get_post_field('post_name', get_post()))); ?>
+        <?php get_template_part(sprintf("templates/produtos/%s/content-9", get_post_field('post_name', get_post()))); ?>
 
     </div>
 
     <div class="js-scroll fade-in-bottom">
-    <?php get_template_part(sprintf("templates/produtos/%s/content-hakon", get_post_field('post_name', get_post()))); ?>
+        <?php get_template_part(sprintf("templates/produtos/%s/content-hakon", get_post_field('post_name', get_post()))); ?>
 
     </div>
 
     <div class="js-scroll fade-in-bottom">
-    <?php get_template_part(sprintf("templates/produtos/%s/content-ethernia-cold", get_post_field('post_name', get_post()))); ?>
+        <?php get_template_part(sprintf("templates/produtos/%s/content-ethernia-cold", get_post_field('post_name', get_post()))); ?>
 
     </div>
 
     <div class="js-scroll fade-in-bottom">
-    <?php get_template_part(sprintf("templates/produtos/%s/content-omer-premium", get_post_field('post_name', get_post()))); ?>
+        <?php get_template_part(sprintf("templates/produtos/%s/content-omer-premium", get_post_field('post_name', get_post()))); ?>
 
     </div>
 
     <div class="js-scroll fade-in-bottom">
-    <?php get_template_part(sprintf("templates/produtos/%s/content-ultramed", get_post_field('post_name', get_post()))); ?>
+        <?php get_template_part(sprintf("templates/produtos/%s/content-ultramed", get_post_field('post_name', get_post()))); ?>
 
     </div>
 
     <div class="js-scroll fade-in-bottom">
-    <?php get_template_part(sprintf("templates/produtos/%s/content-omer-smart", get_post_field('post_name', get_post()))); ?>
+        <?php get_template_part(sprintf("templates/produtos/%s/content-omer-smart", get_post_field('post_name', get_post()))); ?>
 
     </div>
 
@@ -96,6 +96,38 @@
 
     </div>
 
+
+
+
+</section>
+
+<section class="links">
+    <div class="container">
+        <h2>Registro Anvisa: ######</h2>
+
+        <div class="list-links">
+            <div>
+                <a href="" target="_blank"><i class="fa fa-question-circle	"></i> Dúvidas frequentes</a>
+                <a href="" target="_blank"><i class="fa fa-file-text-o"></i> Ficha Técnica</a>
+                <a href="" target="_blank"><i class="fa fa-book	"></i>  Manual de instruções</a>
+            </div>
+            <div>
+                <a href="" target="_blank"><i class="material-icons">contact_phone</i> Assistência express</a>
+                <a href="" target="_blank"><i class="material-icons">library_books</i> Guia Rápido</a>
+                <a href="" target="_blank"><i class="fa fa-file-video-o	"></i>Vídeos de treinamento</a>
+            </div>
+
+
+
+
+
+
+
+
+
+        </div>
+
+    </div>
 </section>
 
 
