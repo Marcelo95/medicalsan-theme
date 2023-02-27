142d1e0dea49ccddd00931522bb5090e88b5ef6b
Marcelo Vieira <marcelovieira1995@gmail.com>
2023-02-27 19:31:07 -0300

    css voltando

--- tema/header.php
+++ tema/header.php
@@ -23,7 +23,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-    <link rel="stylesheet" href="<?php echo asset('css/custom.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo asset('css/custom.css'); ?>">
 
     <?php wp_head(); ?>
 </head>
