4ce09382912c627df5594c39a6f7ec16c5c4c32e
Marcelo Vieira <marcelovieira1995@gmail.com>
2023-03-14 14:12:16 -0300

    ajustes

--- tema/templates/produtos/lipocavity-new/content-video.php
+++ tema/templates/produtos/lipocavity-new/content-video.php
@@ -1,6 +1,25 @@
-<section class="bloco-4-video <?php echo 'css-4-' . get_post_field('post_name', get_post()); ?>">
-    <div class="container">
-    
-    <iframe width="100%" height="400" src="https://www.youtube.com/embed/VepgpHnTCBM?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </div>
-</section>
+<?php
+
+$video = get_post_meta(get_the_ID(), '_metabox_for_produtos_link_videos_de_como_funciona', true);
+
+if ($video) : ?>
+
+
+
+    <section class="js-scroll fade-in-bottom">
+        <div class="videos">
+
+            <div class="init-videos container mt-5 mb-5">
+
+
+                <?php echo cmb2_texto($video); ?>
+
+
+            </div>
+
+
+
+        </div>
+    </section>
+
+<?php endif; ?>
