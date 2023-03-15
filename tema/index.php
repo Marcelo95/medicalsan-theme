92aa9df4cb73336c5f50773f90cf3ef3fba58679
Marcelo Vieira <marcelovieira1995@gmail.com>
2023-03-15 14:59:59 -0300

    home e çupa de buscar

--- tema/index.php
+++ tema/index.php
@@ -3,43 +3,6 @@
 <?php get_template_part("templates/content-home"); ?>
 
 
-
-<section class="content-nossas-tecologia js-scroll fade-in">
-    <div class="container">
-        <div class="slider-slick">
-            <div class="setas slider-1-setas noselect">
-
-                <i class="material-icons seta-esquerda" style="font-size: 36px;margin-left: -7px;">keyboard_arrow_left</i>
-                <h2 class="font-2"><?php echo __("Nossas tecnologias"); ?></h2>
-                <i class="material-icons seta-direita" style="font-size: 36px;margin-left: -7px;">keyboard_arrow_right</i>
-
-            </div>
-
-            <div class="slider-1">
-
-                <?php
-                $marcas = get_terms(array(
-                    'taxonomy' => 'category',
-                    'hide_empty' => false,
-                    'parent' => get_cat_ID('tecnologias'),
-                ));
-
-
-                foreach ($marcas as $key => $item) {
-                    $style_img_bg = asset_image_background(get_term_meta($item->term_id, 'image', true));
-                    echo sprintf('<a href="%s"><div class="marcas-photo font-2" %s >%s</div></a>', esc_url(get_term_link($item)), $style_img_bg, $item->name);
-                }
-
-                ?>
-            </div>
-
-
-        </div>
-
-
-    </div>
-</section>
-
 <section class="box-lancamento js-scroll fade-in estilo-1">
     <div class="container">
         <?php if (is_active_sidebar('home_widget_1')) : ?>
@@ -184,30 +147,6 @@ if(count($itens)): ?>
 
 
 <?php do_action("SelosDeQualidadeComponent");  ?>
-
-
-<section class="mapa-home">
-    <div class="container js-scroll  fade-in-bottom">
-        <div class="col">
-            <div class="box-text-left m-auto me-xl-0">
-                <h2>
-                    <span class="maior">
-                        100
-                    </span>
-                    distribuidores
-                    pelo Brasil
-                </h2>
-
-                <a href="<?php echo home_url("contato");?>" class="botao-1">Faça parte da nossa família</a>
-
-            </div>
-        </div>
-        <div class="col col-small">
-            <img src="<?php echo asset("images/mapa-home.svg"); ?>" alt="Mapa">
-        </div>
-    </div>
-</section>
-
 
 
 <section class="products box-blog js-scroll  fade-in-bottom">
