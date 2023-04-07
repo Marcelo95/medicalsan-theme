a4e713c60e5003f3c3165817ddd528964688cdc0
Marcelo Vieira <marcelovieira1995@gmail.com>
2023-04-07 18:15:50 -0300

    more updates

--- tema/page-sobre.php
+++ tema/page-sobre.php
@@ -1,8 +1,18 @@
 <?php get_header(); ?>
 
 <section class="banner-custom">
-    <div>
-		<div class="image"></div>
+	<div>
+		<div class="image">
+			<?php
+
+			if (has_post_thumbnail()) {
+				the_post_thumbnail();
+			} else {
+				echo '<img src="' . asset('images/bg-page-sobre.png') .'" />';
+			}
+
+			?>
+		</div>
 	</div>
 </section>
 
@@ -23,7 +33,7 @@
 
 <?php do_action("LinhaDoTempoComponent"); ?>
 
-<?php  do_action("NossasMarcasComponent"); ?>
+<?php do_action("NossasMarcasComponent"); ?>
 
 
 <?php get_footer(); ?>
